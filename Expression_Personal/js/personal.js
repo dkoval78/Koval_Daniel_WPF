673764b3f1cc8de5336a63c5f88f33c630b5a789
Dan Koval <kovaldan78@gmail.com>
2014-11-06 18:40:21 -0800

    Alert results

--- Expression_Personal/js/personal.js
+++ Expression_Personal/js/personal.js
@@ -14,21 +14,21 @@ var name = prompt("What is your name?");
 console.log(name);
 
 //Ask how much user pays for each tank of gas
-var dollarsTank = prompt(name + "How much does it cost to fill your gas tank?");
+var dollarsTank = prompt(name + ", how much does it cost to fill your gas tank?");
 
 //Console.log out the response
 console.log(dollarsTank);
 
 //Ask how often they fill their tank per week
 
-var timesMonth = prompt(name + "How many times do you fill your tank per month?");
+var timesMonth = prompt(name + ", how many times do you fill your tank per month?");
 
 //Console.log out the response
 console.log(timesMonth);
 
 //Ask how any months in a year
 
-var monthsYear = prompt(name + "How many months are in a year?");
+var monthsYear = prompt(name + ", how many months are in a year?");
 
 //Console.log out the response
 console.log(monthsYear);
@@ -36,8 +36,8 @@ console.log(monthsYear);
 //Calculate how much they will pay for gas in a year
 var dollarsYear = dollarsTank * timesMonth * monthsYear ;
 
+//Alert user with the dollar amount
+alert(name + " based on your answers, you will pay " + dollarsYear + " this year.");
+
 //Console.log out the response
 console.log(dollarsYear);
-
-//Alert user with the dollar amount
-alert("");
