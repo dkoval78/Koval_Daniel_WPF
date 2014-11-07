e8c35eb113558373993a868bf8de8c71269e0099
Dan Koval <kovaldan78@gmail.com>
2014-11-06 21:23:46 -0800

    Added var

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -38,4 +38,13 @@ var finalInterest = totalOwed * avgInterest;
 
 var finalAmount = totalOwed + finalInterest;
 
+//Console.log out the results
+console.log("Based on this information, the total amount owed will be $" + finalAmount);
+
+//Alert the user of the total amount owed
+alert("Based on this information, the total amount owed will be $" + finalAmount);
+
+//Calculate the monthly payment based on a 10 year repayment plan
+
 //Alert the user how much the amount would be for a 10 year repayment plan
+alert("Over the 10 year repayment plan, your estimated monthly payment would be")
