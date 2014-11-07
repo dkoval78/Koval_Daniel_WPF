462b5bfdb5464bf57eb040d595bf6b602d6b5ff6
Dan Koval <kovaldan78@gmail.com>
2014-11-06 18:09:25 -0800

    First variable
    First variable

--- Expression_Personal/js/personal.js
+++ Expression_Personal/js/personal.js
@@ -5,3 +5,10 @@ WPF section 01
 expressions personal
  */
 
+//Calculate how much user can expect to pay for gas in a year
+//Ask how much user pays for each tank of gas
+
+var dollarsTank = prompt("How much does it cost to fill your gas tank?");
+
+//Console.log out the response
+console.log(dollarsTank);
