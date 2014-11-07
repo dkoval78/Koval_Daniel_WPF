f72c238ee2df27742dabee92a8d5b19d170aa36f
Dan Koval <kovaldan78@gmail.com>
2014-11-06 20:24:36 -0800

    added variables

--- Expressions_Wacky/js/wacky.js
+++ Expressions_Wacky/js/wacky.js
@@ -5,7 +5,7 @@ WPF section 01
 expressions wacky
  */
 
-//To determine how many hours on average the user plays video games per day
+//To determine how many hours on average the user plays video games per day, and how many hours they play per year if they gave up one hour per day
 
 //Ask user how many hours they play per day
 var hoursDay = prompt("How many hours do you play video games per day?");
@@ -20,10 +20,19 @@ var daysWeek = prompt("How many days do you play per week?");
 console.log(daysWeek);
 
 //Ask user how many days are in a year
-var daysYear = prompt("How many days are in a year?");
+var weeksYear = prompt("How many weeks are in a year?");
 
 //Console.log out the response
 console.log(daysYear);
 
+//Calculate the average number of hours spent playing video games per year
+
+var hoursYear = hoursDay * daysWeek / weeksYear;
+
+//Console.log out the results
+console.log("Based on your answers, you spend an average of " + hoursYear + " per day playing video games.");
+
+//Let the user know how many hours they play on average per day
+alert("Based on your answers, you spend an average of " + hoursYear + " per day playing video games.");
 //Let user know what the total would be if they played one less hour per day
 
