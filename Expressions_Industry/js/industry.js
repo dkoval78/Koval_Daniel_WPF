105481d8633e7d34e6fbb6dba64cd8de0c60e5c5
Dan Koval <kovaldan78@gmail.com>
2014-11-06 21:07:59 -0800

    loan terms

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -4,3 +4,9 @@ WPF section 01
 11/6/14
 expressions industry
  */
+
+//To determine how much the user can expect to repay in student loans
+
+//Array of loan terms in years
+var loanTerms = ['10', "15", "25"];
+
