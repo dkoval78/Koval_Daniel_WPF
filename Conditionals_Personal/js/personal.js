569a4c8fd67103f99f2e7704f203c0df3ed5be3d
Dan Koval <kovaldan78@gmail.com>
2014-11-13 19:31:15 -0800

    validation prompt

--- Conditionals_Personal/js/personal.js
+++ Conditionals_Personal/js/personal.js
@@ -12,4 +12,18 @@ expressions personal
 
  var interest = "yes";
 
+ //Determine if the user is old enough to drink.
+
+ var age = prompt("Please tell us your age.");
+
+ console.log(age);
+
+ //Validate prompt to ensure it is not empty.
+
+ if (age === "") {
+
+  age = prompt("Please enter a valid age.");
+ }
+
+ console.log(age);
  
