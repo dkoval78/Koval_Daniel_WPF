d3b523ab7f804df0fde13ca79239f49e89b08b9c
Dan Koval <kovaldan78@gmail.com>
2014-11-13 20:31:52 -0800

    if statement

--- Conditionals_Industry/js/industry.js
+++ Conditionals_Industry/js/industry.js
@@ -6,3 +6,24 @@ WPF section 01
 expressions personal
  */
 
+//To determine if a user is a student and if they are eligible for student loans.
+
+ var student = "yes" || "no";
+
+ var studentStatus = "full time" || "half time";
+
+ //Determine if the user is a student
+
+ var student = prompt("Are you currently a student?");
+
+ console.log(student);
+
+ //Validate prompt to ensure it is not blank.
+
+ if(student === ""){
+
+    student = prompt("Please let us know if you a student, 'yes' or 'no'.");
+ }
+
+ console.log(student);
+ 
