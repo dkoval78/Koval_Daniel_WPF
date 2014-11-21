afa2798a127bc9c9fc4464e85118cde411dcd5db
Dan Koval <kovaldan78@gmail.com>
2014-11-20 16:56:40 -0800

    added variable

--- Functions_Industry/js/industry.js
+++ Functions_Industry/js/industry.js
@@ -6,3 +6,13 @@ WPF section 01
 Functions Industry
  */
 
+//To determine how much of a tuition credit a student would get based on the week they drop.
+
+ var tuition = prompt("Please enter in the total amount of tuition you were charged.");
+
+ //Validate prompt to make sure it is not empty
+
+ while(tuition===""){
+
+    tuition = prompt("Please enter in the total amount of tuition you were charged.");
+ }
