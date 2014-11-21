5786dbeb886ca8213e28bafc1204746873f7005c
Dan Koval <kovaldan78@gmail.com>
2014-11-20 17:12:59 -0800

    added notes and validation

--- Functions_Industry/js/industry.js
+++ Functions_Industry/js/industry.js
@@ -8,6 +8,7 @@ Functions Industry
 
 //To determine how much of a tuition credit a student would get based on the week they drop.
 
+ //Ask for variable tuition to see how much tuition the student was charged
  var tuition = prompt("Please enter the total amount of tuition you were charged.");
 
  //Validate prompt to make sure field is not empty and is actually a number
@@ -17,9 +18,11 @@ Functions Industry
   tuition = prompt("Please enter the total amount of tuition you were charged.");
  }
 
+ //Ask for variable weekDropped to see how long student attended for
  var weekDropped = prompt("Please enter the week you dropped (week 1-10)");
 
- while(isNaN(weekDropped) || weekDropped==="" || weekDropped ==="0" || weekDropped > 10){
+ //Validate prompt to make sure field is not empty and is actually a valid number
+ while(isNaN(weekDropped) || weekDropped==="" || weekDropped === "0" || weekDropped > 10){
 
   weekDropped = prompt("Please enter the week you dropped (week 1-10)");
  }
