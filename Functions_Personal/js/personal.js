6f7a15c2ec8c2d2148c11f110926d75cb43edae4
Dan Koval <kovaldan78@gmail.com>
2014-11-20 18:35:57 -0800

    personal variable

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -6,3 +6,18 @@ WPF section 01
 Functions Personal
  */
 
+//To determine if the user is happy with the waiter/waitress and how much to tip based on bill
+
+ //Ask for how much the overall bill was after the users meal
+
+ var bill = prompt("Please enter in the total amount of your bill.");
+
+ //Validate prompt to ensure that it is a number and is not empty
+
+ while(isNaN(bill) || bill===""){
+
+  bill = prompt("Please enter in the total amount of your bill.")
+ }
+
+ //Console.log the results
+ console.log(bill);
