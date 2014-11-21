72c6149a32828becc5cf98d0f8e8ba020ed10bd1
Dan Koval <kovaldan78@gmail.com>
2014-11-20 19:52:24 -0800

    added variable

--- Functions_Wacky/js/wacky.js
+++ Functions_Wacky/js/wacky.js
@@ -11,9 +11,22 @@ Functions Wacky
  //Ask what the users golf score is
  var golfScore = prompt("What is your golf score after 18 holes?");
 
+ //Validate prompt to ensure it is a number and is not blank
  while(isNaN(golfScore) || golfScore===""){
 
   golfScore = prompt("What is your golf score after 18 holes?");
  }
 
- 
+ //Console.log the results
+ console.log(golfScore);
+
+ //Ask to see if the user is willing to take golf lessons
+
+ golfLessons = prompt("Are you willing to take golf lessons?");
+
+ //Validate prompt to ensure that it is not blank
+ while(golfLessons === ""){
+
+   golfLessons = prompt"Are you willing to take golf lessons? ('yes' no 'no'";
+
+ }
