f6934f886f803b3a1b25c3ca2d0c71cab8b1b6d5
Dan Koval <kovaldan78@gmail.com>
2014-11-20 19:48:55 -0800

    wacky variable

--- Functions_Wacky/js/wacky.js
+++ Functions_Wacky/js/wacky.js
@@ -6,3 +6,14 @@ WPF section 01
 Functions Wacky
  */
 
+//To determine if a golfer can expect to improve his/her game through lessons and club fitting
+
+ //Ask what the users golf score is
+ var golfScore = prompt("What is your golf score after 18 holes?");
+
+ while(isNaN(golfScore) || golfScore===""){
+
+  golfScore = prompt("What is your golf score after 18 holes?");
+ }
+
+ 
